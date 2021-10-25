a8f632ac73f5fca4b12614c31ed9592f06f1d25c
sarala <sarala.sk17@gmail.com>
2021-10-25 13:22:23 +0530

    initial commit

---
+++ src/main/java/com/hackathon2021/decryptionApplication/DecryptionApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon2021.decryptionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon2021/decryptionApplication/DecryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon2021.decryptionApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
